3e471162bc301c0ff6a3bbc7e6554d74e86fffcd
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-23 12:05:42 +0530

    Update WorkingTest.py
    updated many functions still need to be worked upon in many areas so will continue after lunch

--- WorkingTest.py
+++ WorkingTest.py
@@ -1,7 +1,9 @@
-class Player:
-    def __init__ (self, bet, c)
+import random
 
-        self.c = 0
+class Player:
+    def __init__ (self, bet, cards)
+
+        #self.c = 0
         self.bet = bet
         self.pool = 0
         self.cards = {}
@@ -10,12 +12,15 @@ class Player:
         self.splitting = 0
         self.surrender = 0
         self.win = 0
+        self.lose = 0
         self.turn = 0
+        self.l = []
+        self.l_values = []
 
 
     
 
-    def pool_value(self, win, surrender):
+    def pool_value(self):
         if self.surrender == 1:
             self.pool = self.pool - ((self.bet)/2)
 
@@ -26,10 +31,14 @@ class Player:
             self.pool = self.pool - self.bet
 
     def dd_check(self):
-        pass
+        
+        if self.l[1] == self.l[-1]:
+            if self.pool > bet*2:
+
+
 
     def double_betting(self):
-        pass
+        bet = bet*2
 
     def displaying_vertical(self):
         pass
@@ -42,23 +51,16 @@ class Player:
 
     def surrender (self):
         self.pool_value(surrender = 1)
+        return 0
+       # end_menu(bj_check(),bust_check(),winning_check(),surrender())
 
 
 
 
+############################################################################################################
 
-
-bet = int(input('Enter The Betting Value'))
-p = Player(bet,c )
-
-p.c = '1K'
-print(p.bet)
-print(p.c)
-print(p.cards)
-
-
-def turn_check():
-    global obj_list
+def turn_check(obj_list):
+    #global obj_list
 
     for obj in obj_list:
 
@@ -103,7 +105,55 @@ def cards_value(obj):
             
             l_values.append(l[i])
             l_sum = sum(l_values)
-
+        
+        if obj == p:
+            obj.l = l
+            obj.l_values = l_values
+        
         return l_sum
 
+def card_distribution(obj):
+
+    c_value = random.randint(0,13)
+    c_suite =  rand.randint(0,4)*100
+    c = c_value + c_suite    
+    obj.cards[str(c)] = c
+
+def bj_check(obj):
+    #add the changed betting amount setting.
+    if obj.card_value == 21:
+        obj.win = 1
+        return 1
+    else:
+        return 0
+
+def bust_check(obj):
+    
+    if obj.card_value > 21:
+        return 0
+
+    else:
+        return 1
+
+def winning_check(obj_list):
+
+    if obj_list[0].card_value > obj[1].card_value:
+        return 1
+
+    else:
+        return 0
+
+def end_menu():
+    l =[p.win, p.b]
+
+    if l.any(1):
+        won()
+        p.win = 1
+
+    elif l.all(0):
+        lost()
+        p.win = 0
+
+    p.pool_value()
+#revise the end menu fucntion.
 obj = turn_check()
