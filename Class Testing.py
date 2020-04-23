806d8092b2122776627d46debf0559c718e05ab1
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-23 10:22:43 +0530

    Update Class Testing.py
    this here was to test whether paing the object as the argument of the function will change the values or not in the object values

--- Class Testing.py
+++ Class Testing.py
@@ -1,18 +1,19 @@
 class Player:
-    def __init__ (self, bet, c1,c2,c3):
+    def __init__ (self, bet, c):
 
         self.bet = bet
         self.pool = 0
-        self.c1 = c1
-        self.c2 = c2
-        self.c3 = c3
+        self.c = c
+
         self.card_value = 0
         
 bet = int(input('Enter The Betting Value'))
-p = Player(bet, 0 ,0 ,0 )
+p = Player(bet, 0 )
 
-p.c1 = '1K'
-print(p.bet)
-print(p.c1)
+def test(obj):
 
-    
+    obj.c = '1k'
+    return obj.c
+
+p.c = test(p)
+print(p.c)
