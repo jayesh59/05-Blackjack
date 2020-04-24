44335d0dbbeec88b5547062f8190266a294f9824
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-24 09:49:28 +0530

    Update Menu Testing.py
    here the end menu lay out is complete.
    now will work on the game layout but in sepereate fucntiond for each part of it
    starting with the headings.

--- Menu Testing.py
+++ Menu Testing.py
@@ -3,8 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
  
 shape = (720,1680,3)
-black = np.zeros(shape)#dtype =  np.int32)
-#black = cv2.cvtColor(black, cv2.COLOR_BGR2GRAY)
+black = np.zeros(shape)
 
 def video_format_layout():
     #global black
@@ -35,18 +34,26 @@ def start_menu_layout():
         
     return img  
 
-def end_menu_layout():
+def end_menu_layout(obj):
     global black
     img = black.copy()
     y = 0
-    img = np.int32(img)
 
-   #cv2.putText(img,'Victory!!!',(423, 247), cv2.FONT_HERSHEY_COMPLEX, 5, (255,255,255), 10)
-   # cv2.putText(img,'U Surrendered!',(320, 247), cv2.FONT_HERSHEY_COMPLEX, 4, (255,255,255), 10)
-    cv2.putText(img,'Defeat !!!',(423, 247), cv2.FONT_HERSHEY_COMPLEX, 5, (255,255,255), 10)
+    if obj.surrender == 1:
+        cv2.putText(img,'U Surrendered!',(320, 247), cv2.FONT_HERSHEY_COMPLEX, 4, (255,255,255), 10)
 
-    plt.imshow(img, cmap = 'gray')
-    plt.show()
+    elif obj.win == 0:
+        cv2.putText(img,'Defeat !!!',(423, 247), cv2.FONT_HERSHEY_COMPLEX, 5, (255,255,255), 10)
+
+    elif obj.win == 1:
+        cv2.putText(img,'Victory!!!',(423, 247), cv2.FONT_HERSHEY_COMPLEX, 5, (255,255,255), 10)
+   
+   l_options = ['1. Stats Status', '2. Next Round', '3. Exit']
+
+    for string in l_options:
+        cv2.putText(img, string, (600, 400+y), cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255), 7)
+        y= y+67
+
 
 
 
