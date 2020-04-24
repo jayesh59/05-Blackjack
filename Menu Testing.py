a625a5ae2c79830e7eef0a7143fecdcfbee5b403
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-24 10:43:40 +0530

    Update Menu Testing.py
    this file has become a base of displaying functions
    now need to work on game play testing and for that i need other files for easy ebugging and stuff and then will copy it in this stuff
    so will now work and make other files

--- Menu Testing.py
+++ Menu Testing.py
@@ -5,17 +5,25 @@ import matplotlib.pyplot as plt
 shape = (720,1680,3)
 black = np.zeros(shape)
 
-def video_format_layout():
+def video_format_layout(layout, obj = None):
     #global black
     while True:
 
-        black_img = start_menu_layout() 
-        cv2.imshow('Start Menu', black_img)
-
+        cv2.imshow(layout[0], layout[1])
         key = cv2.waitKey(1) & 0xFF 
 
-        if key == 27 or key == ord('3'):
-            break
+
+        if layout[2] == 'a':
+            if key == 27 or key == ord('3'):
+                break
+        
+        elif layout[2] == 'b':
+            if key == 27 or key == ord('3'):
+                break
+
+        elif layout[2] == 'c':
+            if key == 27 or key == ord('3'):
+                break
 
     cv2.destroyAllWindows()
 
@@ -32,7 +40,8 @@ def start_menu_layout():
         cv2.putText(img, string, (600, 400+y), cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255), 7)
         y= y+67
         
-    return img  
+    layout_list = ['Start Menu', img, 'a']
+    return layout_list
 
 def end_menu_layout(obj):
     global black
@@ -54,13 +63,24 @@ def end_menu_layout(obj):
         cv2.putText(img, string, (600, 400+y), cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255), 7)
         y= y+67
 
+    layout_list = ['End Menu', img, 'c']
+    return layout_list
 
+def gameplay_layout(obj):
+    pass
 
+def option_menu_layout(obj):
+    pass
 
+def card_layout(obj):
+    pass
+
+def stats_layout(obj):
+    pass
 
 
 
 
 end_menu_layout()
-#video_format_layout()
+video_format_layout()
 #start_menu_layout() 
