ebf907fc22f2c8ebcab922338b98ca0286915a31
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-24 08:27:36 +0530

    Update Menu Testing.py
    so the assertion error was because the img was in int32 format but it was required to be float to be shown by the cv2.imshow
    and int32 was requied by the plt.imshow to display the img so will remember this for future.

--- Menu Testing.py
+++ Menu Testing.py
@@ -3,14 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
  
 shape = (720,1680,3)
-black = np.zeros(shape, dtype =  np.int32)
+black = np.zeros(shape)#dtype =  np.int32)
 #black = cv2.cvtColor(black, cv2.COLOR_BGR2GRAY)
 
 def video_format_layout():
     #global black
     while True:
 
-        black_img = start_menu_layout()
+        black_img = start_menu_layout() 
+        #print(black_img.shape)
+        #black_img = np.float(black_img)
         cv2.imshow('Start Menu', black_img)
 
         key = cv2.waitKey(1) & 0xFF 
@@ -30,9 +32,10 @@ def start_menu_layout():
     l_options = ['1. Start', '2. Rules', '3. Exit']
 
     for string in l_options:
-        cv2.putText(img, string, (600, 400+y), cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255), 10)
+        cv2.putText(img, string, (600, 400+y), cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255), 7)
         y= y+67
-
+    print(img.shape)
     return img
 
-video_format_layout()    
+video_format_layout()   
+#start_menu_layout() 
