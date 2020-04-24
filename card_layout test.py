7ab0302cb30eacd25a4f9a69e54f0db13bbd3afe
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-24 19:54:13 +0530

    Update card_layout test.py
    this function prints out the card tempplate with suit and it's no.
    
    right now it only gives no. according to the given values adn the spade and all is fixed so now creating conditions now.

--- card_layout test.py
+++ card_layout test.py
@@ -2,15 +2,51 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-shape = (720, 1680)
-img = np.zeros(shape, dtype = np.int32)
-y = 0 
+#Initiation:
+Spade = cv2.imread('Spade.jpg')
+Diamond = cv2.imread('Diamond.jpg')
+Club = cv2.imread('Club.jpg')
+Heart = cv2.imread('Heart.jpg')
 
-l_options = ['1. Start', '2. Rules', '3. Exit']
+shapes = [Spade, Diamond, Club, Heart]
+for s in shapes: _, s = cv2.threshold(s, 127, 255, cv2.THRESH_BINARY)
 
-for string in l_options:
-    cv2.putText(img, string, (600, 400+y), cv2.FONT_HERSHEY_PLAIN, 4, (255,255,255), 7)
-    y= y+67
+resized_shapes = []
+for s in shapes:
+    s_copy = s.copy()
+    s_copy = cv2.resize(s_copy, (0,0), s_copy, 0.2, 0.2)
+    resized_shapes.append(s_copy)
 
-plt.imshow(img)
+
+#Base For Cards:
+shape = (133, 75, 3)
+img = np.ones(shape, dtype = np.uint16) * 255
+img_copy = img.copy
+c = 0
+cards = []
+
+
+for s in resized_shapes:
+    
+    img = np.ones(shape, dtype = np.uint16) * 255
+    img_copy = img.copy()
+    
+    y = int((img.shape[0] - s.shape[0])/2)
+    x = int((img.shape[1] - s.shape[1])/2)
+    w,h = (img.shape[0]-3, img.shape[1]-24)
+
+    img_copy = cv2.putText(img_copy, '10', (0, 15), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,0), 2)
+    
+    img_copy[y:y+s.shape[0], x:x+s.shape[1]] = s
+    cards.append(img_copy)
+    
+
+for i in cards:
+    plt.imshow(i)
+    plt.show()
+
+
+'''
+plt.imshow(cards[0])
 plt.show()
+'''
