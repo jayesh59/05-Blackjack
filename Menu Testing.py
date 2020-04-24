aa81d95fd749c107462521abd9bbb01a95d83273
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-24 07:37:49 +0530

    Update Menu Testing.py
    this is to check the postion required to place the headings at time
    
    the font scale is too large so will try diffetent ones now.

--- Menu Testing.py
+++ Menu Testing.py
@@ -1,17 +1,31 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
  
-shape = (1680, 720)
-black = np.zeros((1680,720,3))
+shape = (720,1680,3)
+black = np.zeros(shape)
 #black = cv2.cvtColor(black, cv2.COLOR_BGR2GRAY)
 
-while True:
+def frame_size():
+    global black
+    while True:
 
-    cv2.imshow('Start Menu', black)
+        cv2.imshow('Start Menu', black)
 
-    key = cv2.waitKey(1) & 0xFF 
+        key = cv2.waitKey(1) & 0xFF 
 
-    if key == 27:
-        break
+        if key == 27:
+            break
 
-cv2.destroyAllWindows()
+    cv2.destroyAllWindows()
+
+def details_coordinates():
+    global black
+    img = black.copy()
+    
+    cv2.putText(img,'BlackJack',(0, 250), cv2.FONT_HERSHEY_COMPLEX, 10, 255, 10)
+    plt.imshow(img, cmap = 'gray')
+    plt.show()
+    
+
+details_coordinates()    
