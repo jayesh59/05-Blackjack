c075b7e6e4f232d66503c515fc2c1c4b83f9f146
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-27 01:06:29 +0530

    Create Menu_Interaction_Testing.py
    this file is to plan and test the sequence of the process of game in correspondance to the composite file
    this file is here till the stay input and the result after that.
    will work on further options tomorrow

---
+++ Menu_Interaction_Testing.py
@@ -0,0 +1,11 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+from Composite_Functions_File import *
+
+
+global p, p2, d
+displaying_starting_window()
+print(type(p))
+displaying_gameplay_window()
+
