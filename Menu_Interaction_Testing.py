481aaab2a3411afcdb90652c737452cfb11bf1ba
jayesh59 <51175318+jayesh59@users.noreply.github.com>
2020-04-27 14:22:52 +0530

    Update Menu_Interaction_Testing.py

--- Menu_Interaction_Testing.py
+++ Menu_Interaction_Testing.py
@@ -1,11 +1,12 @@
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from Composite_Functions_File import *
+from splitting_working_test import *
+
 
 
 global p, p2, d
 displaying_starting_window()
-print(type(p))
+#print(p.cards)
 displaying_gameplay_window()
 
